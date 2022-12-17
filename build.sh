a3d9ed9a1bfaf1cf8d62db5cd49e158e60376a0d
Juan S <segnanajuan@gmail.com>
2022-12-17 19:51:05 -0300

    working. nit

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ echo "Linking pm2..."
 if [ -z "$PM2_KEY" ]; then
   echo "No PM2 key found, please set the PM2_KEY environment variable."
 else
-  pm2 link $PM2_KEY && pm2 start run.sh --name mc_server_bot_ds
+  pm2 link $PM2_KEY
 fi
 
 echo "Installing NPM dependencies..."
